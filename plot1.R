857ea791f8473bad72cf5c662e9da9a3920a12ae
lg.octaviano <lg.octaviano@gmail.com>
2016-08-22 00:14:50 -0300

    Create the plots

---
+++ plot1.R
@@ -0,0 +1,15 @@
+#reading whole dataset
+fulldata <- read.delim("household_power_consumption.txt", sep=";", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
+
+#Datetime conversions
+fulldata$strptime <- strptime(paste(fulldata$Date, fulldata$Time), "%d/%m/%Y %H:%M:%S")
+
+#Subsetting only 2 days worth of data
+date_begin <- strptime("2007-02-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+date_end <- strptime("2007-02-02 23:59:59", "%Y-%m-%d %H:%M:%S")
+twodays <- subset(fulldata, fulldata$strptime>=date_begin & fulldata$strptime<date_end)
+
+#Opening the device and plotting
+png(filename="plot1.png", width = 480, height = 480)
+hist(twodays$Global_active_power, col="orangered", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,21 @@
+#reading whole dataset
+fulldata <- read.delim("household_power_consumption.txt", sep=";", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
+
+#Datetime conversions
+fulldata$strptime <- strptime(paste(fulldata$Date, fulldata$Time), "%d/%m/%Y %H:%M:%S")
+
+#Subsetting only 2 days worth of data
+date_begin <- strptime("2007-02-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+date_end <- strptime("2007-02-02 23:59:59", "%Y-%m-%d %H:%M:%S")
+twodays <- subset(fulldata, fulldata$strptime>=date_begin & fulldata$strptime<date_end)
+
+#Opening the device and plotting
+png(filename="plot3.png", width = 480, height = 480)
+
+#Plotting the data
+plot(twodays$strptime, twodays$Sub_metering_1, type="n", xlab=NA, ylab="Energy sub metering")
+lines(twodays$strptime, twodays$Sub_metering_1,type="l")
+lines(twodays$strptime, twodays$Sub_metering_2,type="l", col="red")
+lines(twodays$strptime, twodays$Sub_metering_3,type="l", col="blue")
+legend('topright', legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=c("solid", "solid", "solid"))
+dev.off()
